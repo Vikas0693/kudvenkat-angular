9cdff420149417aad3b3dfa8d2a0eeb9e11e61b8
vikas0693 <vikas06sharma93@gmail.com>
2020-08-21 00:53:26 +0530

    Added FormGroup

---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employeeForm: FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl(),
+      skills: new FormGroup({
+        skillName: new FormControl(),
+        experienceInYears: new FormControl(),
+        proficiency: new FormControl()
+      })
+    });
+  }
+  onSubmit(): void{
+    console.log(this.employeeForm.value);
+  }
+
+  onLoadDataClick(): void{
+    //in setValue method it is mandatory to set all fields but in patchValue we set subset fields
+    //this.employeeForm.setValue({
+      this.employeeForm.patchValue({
+      fullName: 'Vikas Sharma',
+      email: null,
+      /* skills: {
+        skillName: 'Python',
+        experienceInYears: 2,
+        proficiency: 'beginner'
+      } */
+    });
+  }
+
+}
