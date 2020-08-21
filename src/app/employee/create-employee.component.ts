d91cb65e8d5d1dacde59d0e7b8035abc3c7175f7
vikas0693 <vikas06sharma93@gmail.com>
2020-08-21 17:10:48 +0530

    Added predefined form validators like required,email,minLength,maxLength

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -22,14 +22,15 @@ export class CreateEmployeeComponent implements OnInit {
       })
     }); */
     this.employeeForm = this.fb.group({
-      fullName: ['Default Value'],
-      email: [''],
+      fullName: ['',[Validators.required,Validators.minLength(2), Validators.maxLength(10)]],
+      email: ['',Validators.email],
       skills: this.fb.group({
         skillName: [''],
         experienceInYears: [2],
         proficiency: ['beginner']
       })
     });
+    console.log('Employee Form full name error :',this.employeeForm.get('fullName').errors);
   }
   onSubmit(): void{
     console.log(this.employeeForm.value);
